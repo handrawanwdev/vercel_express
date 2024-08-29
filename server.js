0c56f608c39741d2901b65e84067ee478b84f36b
handrawanwdev <handrawanw.dev@gmail.com>
2024-08-29 21:48:09 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+module.exports = app;
